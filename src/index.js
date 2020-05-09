b85fd6d245bb4b8559fffe4647d4f4a0f4dec90e
y1m1ng1in <43800898+y1m1ng1in@users.noreply.github.com>
2020-05-08 18:59:04 -0700

    project basic setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Form from "./js/components/Form";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const wrapper = document.getElementById("app");
+ReactDOM.render(<Form />, wrapper);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    sourceMapFilename: 'bundle.map'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+    ]
+  },
+};
