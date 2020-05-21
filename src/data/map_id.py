f79b26daab5870ff7cf3053e1dee18d911410ddc
y1m1ng1in <43800898+y1m1ng1in@users.noreply.github.com>
2020-05-20 18:53:30 -0700

    map id to runes

--- src/data/map_id.py
+++ src/data/map_id.py
@@ -1,5 +1,6 @@
 import json
 
+
 def process_champion():
   mapping = {}
   with open('../../assets/dataDragon/champion.json', encoding="utf8") as f:
@@ -14,6 +15,7 @@ def process_champion():
   f.close()
   return mapping
 
+
 def process_item():
   mapping = {}
   with open('../../assets/dataDragon/item.json', encoding="utf8") as f:
@@ -26,6 +28,7 @@ def process_item():
       }
   f.close()
   return mapping
+
 
 def process_summoner():
   mapping = {}
@@ -41,19 +44,48 @@ def process_summoner():
   f.close()
   return mapping
 
+
+def process_perk():
+  mapping = {}
+  with open('../../assets/dataDragon/runesReforged.json', encoding="utf8") as f:
+    data = json.load(f)
+    for perk in data:
+      mapping[int(perk['id'])] = {
+        'key'  : perk['key'],
+        'name' : perk['name'],
+        'image': perk['icon'],
+        'rune' : False
+      }
+      for slot in perk['slots']:
+        for rune in slot['runes']:
+          mapping[int(rune['id'])] = {
+            'key'      : rune['key'],
+            'name'     : rune['name'],
+            'image'    : rune['icon'],
+            'shortDesc': rune['shortDesc'],
+            'longDesc' : rune['longDesc'],
+            'rune'     : True
+          }
+  f.close()
+  return mapping
+
+
 def write_json(fname, json_obj):
   with open(fname, 'w') as f:
     json.dump(json_obj, f)
   f.close()
 
+
 def main():
   champion = process_champion()
   item     = process_item()
   summoner = process_summoner()
+  perk     = process_perk()
 
   write_json("championLookup.json", champion)
   write_json("itemLookup.json", item)
   write_json("summonerLookup.json", summoner)
+  write_json("perkLookup.json", perk)
 
 
 if __name__ == '__main__':
