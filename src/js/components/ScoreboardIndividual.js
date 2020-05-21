4676d7d1f50a8ca557ef3e4b80069a96b416a694
y1m1ng1in <43800898+y1m1ng1in@users.noreply.github.com>
2020-05-20 21:53:48 -0700

    add media query

--- src/js/components/ScoreboardIndividual.js
+++ src/js/components/ScoreboardIndividual.js
@@ -17,17 +17,17 @@ const ScoreboardIndividual = ({ champion, spell, summoner, items }) => {
         <div className={`rune-50-${summoner.perk0} sprite`}></div>
         <div className={`perk-70-${summoner.perkSubStyle} sprite`}></div>
       </div>
-      <div className="scoreboard-individual-info">
-        {summoner.name} <br />
-        {summoner.kills}/{summoner.deaths}/{summoner.assists}
+      <div className="individual-info-and-items">
+        <div className="individual-name">{summoner.name}</div>
+        <div className="individual-kda">{summoner.kills}/{summoner.deaths}/{summoner.assists}</div>
+        {
+          items.map((item, i) => 
+            item !== 0 
+            ? <div key={i} className={`item-45-${item} sprite item-sprite`}></div>
+            : <div key={i} className="empty-item sprite item-sprite"></div>
+          )
+        }
       </div>
-      {
-        items.map((item, i) => 
-          item !== 0 
-          ? <div key={i} className={`item-45-${item} sprite item-sprite`}></div>
-          : <div key={i} className="empty-item sprite item-sprite"></div>
-        )
-      }
       <div className="scoreboard-individual-cs">
         {summoner.cs}<br />
         {summoner.goldEarned}g
